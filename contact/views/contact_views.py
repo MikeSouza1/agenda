dd169855b1c58b9121bf043524ecde994a827644
MikeSouza1 <mikesouza2111@gmail.com>
2023-05-04 15:03:37 -0300

    Fazendo a paginação da agenda.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # from django.http import Http404
-
-# Create your views here.
 
 
 def index(request):
@@ -12,8 +11,12 @@ def index(request):
         .filter(show=True)\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -40,8 +43,12 @@ def search(request):
         )\
         .order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Search - '
     }
 
